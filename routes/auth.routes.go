cdaace6254adafd23ced077f40fc76ee5d57f4bb
dnrahmath <dn.rahmath@gmail.com>
2024-07-24 13:36:58 +0700

    commit test get user

--- routes/auth.routes.go
+++ routes/auth.routes.go
@@ -1,9 +1,14 @@
 package routes
 
 import (
+	"crypto/md5"
 	"easyauthapi/controllers"
 	"easyauthapi/middlewares"
 	"easyauthapi/middlewares/validators"
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,30 +21,71 @@ func AuthRoute(router *gin.RouterGroup, handlers ...gin.HandlerFunc) {
 		ctrl := call.NewController()
 		//==================================
 
+		auth.GET("/set-cookie", func(c *gin.Context) {
+			cookie := &http.Cookie{
+				Name:     "unique-id",
+				Value:    "1234567890", // Harus digenerate secara unik untuk setiap pengguna
+				Expires:  time.Now().Add(365 * 24 * time.Hour),
+				HttpOnly: true,
+			}
+			http.SetCookie(c.Writer, cookie)
+			c.JSON(http.StatusOK, gin.H{"status": "cookie set"})
+		})
+
 		auth.GET(
 			"/get-info",
 			func(c *gin.Context) {
-				ip := c.ClientIP()
+				var ip string
+
+				// Mendapatkan IP dari header "X-Forwarded-For"
+				XForwardedFor := c.GetHeader("X-Forwarded-For")
+				if XForwardedFor != "" {
+					ip = XForwardedFor
+					fmt.Println("X-Forwarded-For : " + ip)
+				}
+
+				// Jika "X-Forwarded-For" tidak tersedia, gunakan "X-Real-IP"
+				XRealIP := c.GetHeader("X-Real-IP")
+				if ip == "" && XRealIP != "" {
+					ip = XRealIP
+					fmt.Println("X-Real-IP : " + ip)
+				}
+
+				// Jika header tidak tersedia, gunakan metode bawaan ClientIP [Carrier-Grade NAT (CGNAT)]
+				ClientIP := c.ClientIP()
+				if ip == "" && ClientIP != "" {
+					ip = ClientIP
+					fmt.Println("Client IP : " + ip)
+				}
+
 				userAgent := c.GetHeader("User-Agent")
 				referer := c.GetHeader("Referer")
 				acceptLanguage := c.GetHeader("Accept-Language")
 
-				// Mendapatkan semua cookies yang dikirim oleh klien
-				cookies := c.Request.Cookies()
+				// Generate fingerprint
+				fingerprintSource := ip + userAgent + referer + acceptLanguage
+				hash := md5.Sum([]byte(fingerprintSource))
+				fingerprint := hex.EncodeToString(hash[:])
 
-				// Membuat map untuk menyimpan nama dan nilai cookie
+				cookies := c.Request.Cookies()
 				cookieMap := make(map[string]string)
 				for _, cookie := range cookies {
 					cookieMap[cookie.Name] = cookie.Value
 				}
 
-				// Mengirimkan informasi dalam format JSON
-				c.JSON(200, gin.H{
-					"ip":              ip,
+				headers := c.Request.Header
+
+				c.JSON(http.StatusOK, gin.H{
+					"XForwardedFor":   XForwardedFor,
+					"XRealIP":         XRealIP,
+					"ClientIP":        ClientIP,
+					"SelectedIP":      ip, // IP yang dipilih berdasarkan prioritas
 					"user_agent":      userAgent,
 					"referer":         referer,
 					"accept_language": acceptLanguage,
+					"fingerprint":     fingerprint,
 					"cookies":         cookieMap,
+					"headers":         headers,
 				})
 			},
 		)
